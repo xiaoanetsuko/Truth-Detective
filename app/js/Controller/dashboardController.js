09660db4c2945e19e7124d621487fa362f4b8345
mirrokle <curtaincall1025@gmail.com>
2015-10-26 14:02:23 +1000

    dashboard fully functioning toggle between locked img and unlocked img toggle between chapter title and 'Locked'

--- app/js/Controller/dashboardController.js
+++ app/js/Controller/dashboardController.js
@@ -13,6 +13,13 @@ angular.module('TruthDetective')
             var allChInfo = userService.getInfo();
             console.log(allChInfo);
             for (var i=0;i<allChInfo.length;i++){
+                if (allChInfo[i].username==$scope.username && allChInfo[i].chapter=='tutorial'){
+                    $scope.chTutImg = 'resource/img/site/t.jpg';
+                    $scope.chTutName = 'House Gate';
+                } else {
+                    $scope.chTutImg = lockedImg;
+                    $scope.chTutName = 'Locked';
+                }
                 if (allChInfo[i].username==$scope.username && allChInfo[i].chapter=='one'){
                     $scope.chOneImg = 'resource/img/site/1.jpg';
                     $scope.chOneName = 'Art Gallery';
@@ -20,17 +27,35 @@ angular.module('TruthDetective')
                     $scope.chOneImg = lockedImg;
                     $scope.chOneName = 'Locked';
                 }
+                if (allChInfo[i].username==$scope.username && allChInfo[i].chapter=='two'){
+                    $scope.chTwoImg = 'resource/img/site/1.jpg';
+                    $scope.chTwoName = '';
+                } else {
+                    $scope.chTwoImg = lockedImg;
+                    $scope.chTwoName = 'Locked';
+                }
+                if (allChInfo[i].username==$scope.username && allChInfo[i].chapter=='three'){
+                    $scope.chThreeImg = 'resource/img/site/1.jpg';
+                    $scope.chThreeName = '';
+                } else {
+                    $scope.chThreeImg = lockedImg;
+                    $scope.chThreeName = 'Locked';
+                }
             }
 
             $scope.username = $rootScope.globals.currentUser.username;
-
-            $scope.chOneImg =
 
             $scope.openTutorial = function () {
                 $window.location.href = "http://deco1800-g51.uqcloud.net/tutorial.html";
             };
             $scope.openChOne = function () {
                 $window.location.href = "http://deco1800-g51.uqcloud.net/tutorial.html";
+            };
+            $scope.openChTwo = function () {
+                $window.location.href = "http://deco1800-g51.uqcloud.net/chapter2.html";
+            };
+            $scope.openChThree = function () {
+                $window.location.href = "http://deco1800-g51.uqcloud.net/chapter3.html";
             };
 
 
