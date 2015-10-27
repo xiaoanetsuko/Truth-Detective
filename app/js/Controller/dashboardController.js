e99c7f07f9a6cc55a33c3a34c25dab4f90f77245
mirrokle <curtaincall1025@gmail.com>
2015-10-27 17:06:40 +1000

    more dialogs

--- app/js/Controller/dashboardController.js
+++ app/js/Controller/dashboardController.js
@@ -25,6 +25,45 @@ angular.module('TruthDetective')
             $scope.chTwoName = lockedTitle;
             $scope.chThreeName = lockedTitle;
 
+            $scope.openChOne = function (ev) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element(document.querySelector('#mainBody')))
+                        .clickOutsideToClose(true)
+                        .title('Chapter One is Locked')
+                        .content('To unlock, please complete the Tutorial Chapter.')
+                        //.ariaLabel('Alert Dialog Demo')
+                        .ok('Close')
+                        .targetEvent(ev)
+                );
+            };
+
+            $scope.openChTwo = function (ev) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element(document.querySelector('#mainBody')))
+                        .clickOutsideToClose(true)
+                        .title('Chapter Two is Locked')
+                        .content('To unlock, please complete Chapter One.')
+                        //.ariaLabel('Alert Dialog Demo')
+                        .ok('Close')
+                        .targetEvent(ev)
+                );
+            };
+
+            $scope.openChThree = function (ev) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element(document.querySelector('#mainBody')))
+                        .clickOutsideToClose(true)
+                        .title('Chapter Three is Locked')
+                        .content('To unlock, please complete Chapter Two.')
+                        //.ariaLabel('Alert Dialog Demo')
+                        .ok('Close')
+                        .targetEvent(ev)
+                );
+            };
+
             for (var i=0;i<allChInfo.length;i++){
                 if (allChInfo[i].username == $scope.username) {
                     userChInfo.push(allChInfo[i].chapter)
@@ -50,66 +89,64 @@ angular.module('TruthDetective')
                 $scope.openChOne = function () {
                     $window.location.href = "http://deco1800-g51.uqcloud.net/chapter1.html";
                 };
-            } else {
-                $scope.openChOne = function (ev) {
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                            .parent(angular.element(document.querySelector('#mainBody')))
-                            .clickOutsideToClose(true)
-                            .title('Chapter One is Locked')
-                            .content('To unlock, please complete the Tutorial Chapter.')
-                            //.ariaLabel('Alert Dialog Demo')
-                            .ok('Close')
-                            .targetEvent(ev)
-                    );
-                }
             }
 
-            if (userChInfo.indexOf('two') > -1) {
+            if (userChInfo.length == 2) {
+                $scope.chOneImg = 'resource/img/site/1.jpg';
+                $scope.chOneName = 'Art Gallery';
                 $scope.chTwoImg = 'resource/img/site/1.jpg';
                 $scope.chTwoName = '';
-                $scope.barWidth+=25;
-            } else {
-                $scope.chTwoImg = lockedImg;
-                $scope.chTwoName = lockedTitle;
+                $scope.barWidth+=50;
+                $scope.openChOne = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter1.html";
+                };
+                $scope.openChTwo = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter2.html";
+                };
             }
 
-            if (userChInfo.indexOf('three') > -1) {
+            if (userChInfo.length == 3) {
+                $scope.chOneImg = 'resource/img/site/1.jpg';
+                $scope.chOneName = 'Art Gallery';
+                $scope.chTwoImg = 'resource/img/site/1.jpg';
+                $scope.chTwoName = '';
                 $scope.chThreeImg = 'resource/img/site/1.jpg';
                 $scope.chThreeName = '';
-                $scope.barWidth+=25;
-            } else {
-                $scope.chThreeImg = lockedImg;
-                $scope.chThreeName = lockedTitle;
+                $scope.barWidth+=75;
+                $scope.openChOne = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter1.html";
+                };
+                $scope.openChTwo = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter2.html";
+                };
+                $scope.openChThree = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter3.html";
+                };
+            }
+
+            if (userChInfo.length == 4) {
+                $scope.chOneImg = 'resource/img/site/1.jpg';
+                $scope.chOneName = 'Art Gallery';
+                $scope.chTwoImg = 'resource/img/site/1.jpg';
+                $scope.chTwoName = '';
+                $scope.chThreeImg = 'resource/img/site/1.jpg';
+                $scope.chThreeName = '';
+                $scope.barWidth+=100;
+                $scope.openChOne = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter1.html";
+                };
+                $scope.openChTwo = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter2.html";
+                };
+                $scope.openChThree = function () {
+                    $window.location.href = "http://deco1800-g51.uqcloud.net/chapter3.html";
+                };
             }
 
             $scope.username = $rootScope.globals.currentUser.username;
 
             $scope.openTutorial = function () {
                 $window.location.href = "http://deco1800-g51.uqcloud.net/tutorial.html";
-            };
-            //$scope.openChOne = function () {
-            //    if (userChInfo.length == 1) {
-            //        $window.location.href = "http://deco1800-g51.uqcloud.net/chapter1.html";
-            //    } else {
-            //        $mdDialog.show(
-            //            $mdDialog.alert()
-            //                .parent(angular.element(angular.element(document.body)))
-            //                .clickOutsideToClose(true)
-            //                .title('Chapter One is Locked')
-            //                .content('Please complete Tutorial Chapter first.')
-            //                .ariaLabel('Alert Dialog Demo')
-            //                .ok('Got it!')
-            //                .targetEvent(ev)
-            //        );
-            //    }
-            //
-            //};
-            $scope.openChTwo = function () {
-                $window.location.href = "http://deco1800-g51.uqcloud.net/chapter2.html";
-            };
-            $scope.openChThree = function () {
-                $window.location.href = "http://deco1800-g51.uqcloud.net/chapter3.html";
             };
 
             $scope.progressPercentage = $scope.barWidth+'%';
