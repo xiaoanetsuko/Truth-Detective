c721db6b7e8bb5ba44220a413ddfe4653a08df10
Jianghui Zhang <xiaoanetsuko@hotmail.com>
2015-10-29 10:15:40 +1000

    Editing clue content

--- app/js/Controller/imgcontroller.js
+++ app/js/Controller/imgcontroller.js
@@ -11,13 +11,13 @@ angular.module('TruthDetective')
         $scope.letterT = function () {
             $scope.title = "A postcard of Eiffel Tower";
             $scope.image = "img/img_source/eiffel.jpg";
-            $scope.content = "This is a postcard, but the whole text is already too vague to be read. Luckily, the picture in the back side is still clear. It is an old picture of Eiffel Tower. I heard The Eiffel Tower is a tall wrought-iron structure made by man. It is one of the most famous monuments of the world. Besides, it is the most famous landmark of Europe, held the record as the tallest man-made structure of the world from 1889 to 1930 with its height of 324 meters. The Eiffel Tower is named after the owner of the company who designed and built the tower, Engineer Gustave Eiffel. However, I am wondering where the tower located in?"
+            $scope.content = "This is a postcard, but the whole text is already too vague to be read. Luckily, the picture in the back is still clear. It is an old picture of Eiffel Tower. I heard The Eiffel Tower is a tall wrought-iron structure made by man. It is one of the most famous monuments of the world. Besides, it is the most famous landmark of Europe, held the record as the tallest man-made structure of the world from 1889 to 1930 with its height of 324 meters. The Eiffel Tower is named after the owner of the company who designed and built the tower, Engineer Gustave Eiffel. However, I am wondering where the tower located in?"
         };
 
         $scope.newspaperT = function () {
             $scope.title = "The Most Famous Novel";
             $scope.image = "img/img_source/musketeers.jpg";
-            $scope.content = "This is an old newspaper. The headline for that day is a famous novel called the Three Musketeers. This book is typical of Dumas's works: quick-witted heroes who fight and love unceasingly, fast-paced narrative, and entertaining dialogue. In its romantic subject matter, the book is typical of its time; what is not typical is the fact that it has survived and remains entertaining and accessible for modern readers. The story was drawn from a number of original historical sources, including memoirs of events from the period in which the novel takes place. Dumas's collaborator, Auguste Maquet, brought him a rough scenario for a book set during the reign of King Louis XIII and starring the King, Queen Anne, Cardinal Richelieu, and the Duke of Buckingham. Because Dumas's works have been so wildly popular, for a long time he was not considered a 'serious' writer. However, in recent years, more attention has been given to him because his work laid the foundations for bourgeois drama as he brought history alive for a broad segment of the population who otherwise would have had no interest in it and as he created a new kind of Romantic novel."
+            $scope.content = "This is an old newspaper. The headline for that day is a famous novel called the Three Musketeers in that country. This book is typical of Dumas's works: quick-witted heroes who fight and love unceasingly, fast-paced narrative, and entertaining dialogue. In its romantic subject matter, the book is typical of its time; what is not typical is the fact that it has survived and remains entertaining and accessible for modern readers. The story was drawn from a number of original historical sources, including memoirs of events from the period in which the novel takes place. Dumas's collaborator, Auguste Maquet, brought him a rough scenario for a book set during the reign of King Louis XIII and starring the King, Queen Anne, Cardinal Richelieu, and the Duke of Buckingham. Because Dumas's works have been so wildly popular, for a long time he was not considered a 'serious' writer. However, in recent years, more attention has been given to him because his work laid the foundations for bourgeois drama as he brought history alive for a broad segment of the population who otherwise would have had no interest in it and as he created a new kind of Romantic novel."
         };
 
         $scope.mapT = function () {
@@ -29,7 +29,7 @@ angular.module('TruthDetective')
         $scope.Louis_16 = function () {
             $scope.title = "Second Owner of the Hope";
             $scope.image = "../img/img_source/louis16.jpg";
-            $scope.content = "He is descendent of the Sun King and ruled from 1774 to 1792. During his reign, he tried to reform the country in accordance with Enlightenment ideals. These included efforts to abolish serfdom, remove the taille, and increase tolerance toward non-Catholics. He was determined to be a good king, but his indecisiveness and the overwhelming revolutionary mood among the people failed him. He thus had a tragic ending."
+            $scope.content = "This is the portrait of the second owner of the Hope. He is descendent of the Sun King and ruled from 1774 to 1792. During his reign, he tried to reform the country in accordance with Enlightenment ideals. These included efforts to abolish serfdom, remove the taille, and increase tolerance toward non-Catholics. He was determined to be a good king, but his indecisiveness and the overwhelming revolutionary mood among the people failed him. He thus had a tragic ending in the France Revolution."
         };
 
         $scope.bastille = function () {
@@ -41,7 +41,7 @@ angular.module('TruthDetective')
         $scope.Louis_14 = function () {
             $scope.title = "First Owner of the Hope";
             $scope.image = "../img/img_source/placeholder.png";
-            $scope.content = "He was one of France's most important monarchs and the first owner of the Hope. He ruled from 1643 to 1715 and was known as the Sun King. His childhood was marked by the political troubles of the Fronde, so throughout his reign he sought to maintain the unity of the kingdom at all cost, crushing any moves the aristocracy made to obtain extra power. In 1661, after the death of Cardinal Mazarin, finally free of his influence, he wielded absolute power, but was shrewd enough to surround himself with devoted and talented ministers. The royal purse, the army, industry, and administration were all reformed and trade blossomed. He also greatly encouraged the arts. The chateau of Versailles is the finest achievement of his reign.";
+            $scope.content = "He was one of France's most important monarchs and the first owner of the Hope. He ruled from 1643 to 1715 and was known as the Sun King. His childhood was marked by the political troubles of the Fronde, so throughout his reign he sought to maintain the unity of the kingdom at all cost, crushing any moves the aristocracy made to obtain extra power. In 1661, after the death of Cardinal Mazarin, finally free of his influence, he wielded absolute power, but was shrewd enough to surround himself with devoted and talented ministers. The royal purse, the army, industry, and administration were all reformed and trade blossomed. He also greatly encouraged the arts. The chateau of Versailles is the finest achievement of his reign. After his dead, the grandson Louis XVI inherited the Hope Diamond.";
         };
 
         $scope.coin = function () {
@@ -53,7 +53,7 @@ angular.module('TruthDetective')
         $scope.frenchRevo = function () {
             $scope.title = "An episode in French Revolution";
             $scope.image = "../img/img_source/placeholder.png";
-            $scope.content = "This painting implied an important historic event. It was an influential period of social and political upheaval in France during the reign of the diamond’s second owner and was partially carried forward by Napoleon during the later expansion of the French Empire. The event overthrew the monarchy, established a republic, experienced violent periods of political turmoil, and finally culminated in a dictatorship by Napoleon that rapidly brought many of its principles to Western Europe and beyond. Inspired by liberal and radical ideas, the event profoundly altered the course of modern history, triggering the global decline of absolute monarchies while replacing them with republics.";
+            $scope.content = "This painting implied an important historic event. It was an influential period of social and political upheaval in France during the reign of the diamond’s second owner and was partially carried forward by Napoleon during the later expansion of the French Empire. The event overthrew the monarchy, established a republic, experienced violent periods of political turmoil, and finally culminated in a dictatorship by Napoleon that rapidly brought many of its principles to Western Europe and beyond. Inspired by liberal and radical ideas, the event profoundly altered the course of modern history, triggering the global decline of absolute monarchies while replacing them with republics. The Hope Diamond was lost during this period.";
 
 
         };
@@ -64,15 +64,15 @@ angular.module('TruthDetective')
         };
 
         $scope.coinsGeorge = function () {
-            $scope.title = "";
+            $scope.title = "George IV Coin Collection";
             $scope.image = "../img/img_source/placeholder.png";
-            $scope.content = "Rex Nan Kivell Collection NK5378. Part of the collection: Eighteenth and nineteenth century commemorative medals. Condition: Good. Title devised by cataloguer from medal inscription. Inscriptions on medal: Obverse -- Georgius IIII D. G. Britanniarum Rex F. D.; Reverse -- Proprio Jam Jure Animo Paterno. Inauguratus Die. Julii. XIX Anno. MDCCCXXI."
+            $scope.content = "This coin is called the Rex Nan Kivell Collection NK5378. It was the eighteenth and nineteenth century commemorative medals. Title devised by cataloguer from medal inscription. Inscriptions on medal: Obverse -- Georgius IIII D. G. Britanniarum Rex F. D.; Reverse -- Proprio Jam Jure Animo Paterno. Inauguratus Die. Julii. XIX Anno. MDCCCXXI."
         };
 
         $scope.bridge1760 = function () {
-            $scope.title = "A old Bridge in 1760s";
+            $scope.title = "An old Bridge in 1760s";
             $scope.image = "../img/img_source/bridge1970.jpg";
-            $scope.content = "This is an old painting. The title is burnt because of some reason. This painting is painted by 1760s. At the back, it says: in 1760s, the author of Gephyralogia: An Historical Account of Bridges, Antient and Modern described this bridge as a curiosity “admired by foreigners, and, in most respects, as remarkable as any in Europe”. From 1758 to 1762, there were numeous houses has been built on the bridge.The whole is covered on each side with houses so disposed as to have the appearance of a continued street, not at all of a bridge. However, due to the potential fire issue, all houses and shops on the bridge were demolished through Act of Parliament. The two centre arches were replaced by a single wider span to improve navigation on the river. What is this bridge?"
+            $scope.content = "This is an old painting by 1760s. The title is burnt because of some reason. At the back, the painting stated: in 1760s, the author of Gephyralogia: An Historical Account of Bridges, Antient and Modern described this bridge as a curiosity “admired by foreigners, and, in most respects, as remarkable as any in Europe”. From 1758 to 1762, there were numeous houses has been built on the bridge.The whole is covered on each side with houses so disposed as to have the appearance of a continued street, not at all of a bridge. However, due to the potential fire issue, all houses and shops on the bridge were demolished through Act of Parliament. The two centre arches were replaced by a single wider span to improve navigation on the river. What is this bridge?"
         };
 
         $scope.Buckingham_palace = function () {
@@ -84,7 +84,7 @@ angular.module('TruthDetective')
         $scope.George_IV_1821_color = function () {
             $scope.title = "George IV";
             $scope.image = "../img/img_source/George_IV_1821_color.jpg";
-            $scope.content = "A portrait card for George IV, the third owner of the Hope Diamond. He was King of the United Kingdom of Great Britain and Ireland and of Hanover following the death of his father, George III. George IV led an extravagant lifestyle that contributed to the fashions of the Regency era. He was a patron of new forms of leisure, style and taste. He commissioned John Nash to build the Royal Pavilion inBrighton and remodel Buckingham Palace, and Sir Jeffry Wyattville to rebuild Windsor Castle. He was instrumental in the foundation of theNational Gallery and King's College London. Does he have any relationship with the city from our question?"
+            $scope.content = "A portrait card for George IV, the third owner of the Hope Diamond. He was King of the United Kingdom of Great Britain and Ireland and of Hanover following the death of his father, George III. George IV led an extravagant lifestyle that contributed to the fashions of the Regency era. He was a patron of new forms of leisure, style and taste. He commissioned John Nash to build the Royal Pavilion inBrighton and remodel Buckingham Palace, and Sir Jeffry Wyattville to rebuild Windsor Castle. He was instrumental in the foundation of the National Gallery and King's College London. Does he have any relations with the city from our question?"
         };
 
         $scope.Industrial_Revolution = function () {
@@ -96,7 +96,7 @@ angular.module('TruthDetective')
         $scope.Great_Exhibition = function () {
             $scope.title = "Great Exhibition";
             $scope.image = "../img/img_source/placeholder.png";
-            $scope.content = "The exhibition was held during a period of economic boom and social change. Britain was expanding, the population had grown from twenty seven million in 1851 to thirty two million in 1871. Britain supplied the railways to the world. In 1869 three hundred thousand tonnes of iron went to the USA alone. The Great Exhibition was partly held to show this prosperity. It was an extension of the trade show idea manufactures in France had used to gain custom. The Great Exhibition also held exhibits from other countries with the intention of showing a dominance, it stated in no uncertain terms that Great Britain was the centre of the world in terms of industry. The Queen, Victoria, recognised how well the Great Exhibition did this 'God bless my dear country which has shown itself so great today.' The exhibition showed the exact state of British industry and was the meant to display the influence the British industries had on the world. How Britain was described was as the 'Workshop of the World.' ";
+            $scope.content = "In this city, the exhibition was held during a period of economic boom and social change. Britain was expanding, the population had grown from twenty seven million in 1851 to thirty two million in 1871. Britain supplied the railways to the world. In 1869 three hundred thousand tonnes of iron went to the USA alone. The Great Exhibition was partly held to show this prosperity. It was an extension of the trade show idea manufactures in France had used to gain custom. The Great Exhibition also held exhibits from other countries with the intention of showing a dominance, it stated in no uncertain terms that Great Britain was the centre of the world in terms of industry. The Queen, Victoria, recognised how well the Great Exhibition did this 'God bless my dear country which has shown itself so great today.' The exhibition showed the exact state of British industry and was the meant to display the influence the British industries had on the world. How Britain was described was as the 'Workshop of the World.' ";
         };
 
         $scope.Urban_Working = function () {
@@ -108,7 +108,7 @@ angular.module('TruthDetective')
         $scope.Population_Explosion = function () {
             $scope.title = "Population explosion in England";
             $scope.image = "../img/img_source/population_growth.jpg";
-            $scope.content = "The most prolific evidence of the Industrial Revolution’s impact on the modern world is seen in the worldwide human population growth. Human population growth is indelibly tied together with increased use of natural and man-made resources, energy, land for growing food and for living, and waste by-products that are disposed of, to decompose, pollute or be recycled. This exponential population growth led to the exponential requirements for resources, energy, food, housing and land, as well as the exponential increase in waste by-products.";
+            $scope.content = "The most prolific evidence of this event impact on the modern world is seen in the worldwide human population growth. Human population growth is indelibly tied together with increased use of natural and man-made resources, energy, land for growing food and for living, and waste by-products that are disposed of, to decompose, pollute or be recycled. This exponential population growth led to the exponential requirements for resources, energy, food, housing and land, as well as the exponential increase in waste by-products.";
         };
 
         $scope.Political_Changes = function () {
@@ -126,7 +126,7 @@ angular.module('TruthDetective')
         $scope.steam = function () {
             $scope.title = "Steam Replacing Water Power";
             $scope.image = "../img/img_source/steam.jpg";
-            $scope.content = "The steam engine was one of the most important technologies of the Industrial Revolution, although steam did not replace water power in importance in Britain until after the Industrial Revolution. From Englishman Thomas Newcomen's atmospheric engine, of 1712, through major developments by Scottish inventor and mechanical engineer James Watt, the steam engine began to be used in many industrial settings, not just in mining, where the first engines had been used to pump water from deep workings. Early mills had run successfully with water power, but by using a steam engine a factory could be located anywhere, not just close to water. The development of the stationary steam engine was an essential early element of the Industrial Revolution, however it should be remembered that for most of the period of the Industrial Revolution the majority of industries still relied on wind and water power as well as horse and man-power for driving small machines.";
+            $scope.content = "The steam engine was one of the most important technologies of the Industrial Revolution, although steam did not replace water power in importance in Britain until after revolution of the whole industry. From Englishman Thomas Newcomen's atmospheric engine, of 1712, through major developments by Scottish inventor and mechanical engineer James Watt, the steam engine began to be used in many industrial settings, not just in mining, where the first engines had been used to pump water from deep workings. Early mills had run successfully with water power, but by using a steam engine a factory could be located anywhere, not just close to water. The development of the stationary steam engine was an essential early element of the Industrial Revolution, however it should be remembered that for most of the period of the Industrial Revolution the majority of industries still relied on wind and water power as well as horse and man-power for driving small machines.";
         };
 
         $scope.devMap = function () {
